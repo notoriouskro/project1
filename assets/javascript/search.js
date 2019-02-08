03cf63943406894b23ac9ed2baaaa08ecf3be296
Edward Hopper <burson.kate@gmail.com>
2019-02-07 20:24:55 -0800

    search function working. list filtering still doesn't work

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -14,140 +14,135 @@ var database = firebase.database();
 
 function dropdown() {
     for(var i = 0; i < parkNames.length; i++) {
-        var $p = $('<p>');
+        var $p = $('<p class="list">');
         $p.text(parkNames[i]);
         $('#myDropdown').append($p);
     }
 };
 
-
-
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    $('#myDropdown').toggle('show');
     dropdown();
 };
-  
-function filterFunction() {
-    var input, filter, p;
-    input = $('#myInput').val();
-    filter = input.toUpperCase();
-    div = $('#myDropdown');
-    p = $('<p>');
-    for (var i = 0; i < p.length; i++) {
-        txtValue = p.val();
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        p.css({'display': ''});
-        } else {
-        p.css({'display':'none'});
-        }
-    }
+
+// function filterFunction() {
+//     var input = $('#myInput').val();
+//     var filter = input.toUpperCase();
+//     for (var i = 0; i < parkNames.length; i++) {
+//         var txtValue = $('<p>').val();
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         $('<p>').css({'display': ''});
+//         } else {
+//         $('<p>').css({'display':'none'});
+//         }
+//     }
+// };
+
+// $('#myInput').on('keyup', filterFunction);
+
+function parks(element) {
+    var input = element.text();
+    console.log(input);
+
+    var position = parkNames.indexOf(input);
+    var parkCode = parkCodes[position];
+    // var parkCode = $('#park-search-val').val().substring(0,4);
+    console.log(parkCode);
+
+    $addBtn = $('<button>');
+    $addBtn.attr({'park-name': input, 'park-code': parkCode});
+
+    var queryURL = 'https://developer.nps.gov/api/v1/parks?parkCode=' + parkCode + '&api_key=7saAAebFIxUpWP1IHtyJN3nKfo94xMzf009LSiHb';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        }).then(function(response){
+            console.log(response);
+        
+            var name = response.data[0].name;
+            var $name = $('<h1>');
+            $name.text(name);
+
+            var description = $('<p>' + response.data[0].description + '</p>');
+            var directions = $('<p>' + response.data[0].directionsInfo + '</p>');
+            var directionsURL = $('<p>' + response.data[0].directionsUrl + '</p>');
+            var weather = $('<p>' + response.data[0].weatherInfo + '</p>');
+            var website = $('<p>' + response.data[0].url + '</p>');
+            var $info = $('<div id="info">');
+            $info.append(description, directions, directionsURL, weather, website);
+
+            $('#search-results').append($name, $info);
+            latLong = response.data[0].latLong;
+
+            trails();
+        });
 };
 
-// appObj.lastSearchCode = '';
-// appObj.lastParklat = '';
-// appObj.lastParklong = '';
-// appObj.$addBtn;
+function unsplash(element) {
+    var input = element.text();
+    console.log('unsplash: ' + input);
 
-// function parks() {
-//     var input = $('#park-search-val').val();
+    var queryURL = 'https://api.unsplash.com/search/photos?page=1&query=' + input + '&client_id=595205d0fab64dca9acc4912f7319d2869c29ff0834538b31167dbca9425a2f6&client_secret=00c14faa873902ff8dd494014545db17655595508ae0c67fe37a8774e4b7b45c';
 
-//     var parkCode = $('#park-search-val').val().substring(0,4);
-//     console.log(parkCode);
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        }).then(function(response){
+            console.log(response);
+            for(var i = 0; i < 1; i++){
+            $('#search-results').append('<img src="' + response.results[i].urls.regular + '">');
+            }
+        });
+};
 
-//     $addBtn = $('<button>');
-//     $addBtn.attr({'park-name': input, 'park-code': parkCode});
+function trails() {
+    var input = latLong.split(',');
+    console.log(input);
+    var lat = input[0].substring(4,16);
+    console.log('latitude: ' + lat);
+    var long = input[1].substring(6,18);
+    console.log('longitude: ' + long);
 
-//     var queryURL = 'https://developer.nps.gov/api/v1/parks?parkCode=' + parkCode + '&api_key=7saAAebFIxUpWP1IHtyJN3nKfo94xMzf009LSiHb';
+    $addBtn.attr({'latitude': lat, 'longitude': long});
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//         }).then(function(response){
-//             console.log(response);
-        
-//             var name = response.data[0].name;
-//             var $name = $('<h1>');
-//             $name.text(name);
+    var queryURL = 'https://www.hikingproject.com/data/get-trails?lat='+ lat + '&lon=' + long + '&maxDistance=10&key=200415723-df92bbbf592b6baa4ec5ef44ab0ffed8';
 
-//             var description = $('<p>' + response.data[0].description + '</p>');
-//             var directions = $('<p>' + response.data[0].directionsInfo + '</p>');
-//             var directionsURL = $('<p>' + response.data[0].directionsUrl + '</p>');
-//             var weather = $('<p>' + response.data[0].weatherInfo + '</p>');
-//             var website = $('<p>' + response.data[0].url + '</p>');
-//             var $info = $('<div id="info">');
-//             $info.append(description, directions, directionsURL, weather, website);
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        crossOrigin: true,
+        }).then(function(response){
+            console.log(response);
+            for(var i = 0; i < response.trails.length; i++){
+                var $trails = $('<div id="trails">');
 
-//             $('#search-results').append($name, $info);
-//             latLong = response.data[0].latLong;
+                var $img = $('<img id="trail-img" src="' + response.trails[i].imgSqSmall + '">');
+                $trails.append($img);
 
-//             trails();
-//         });
-// };
+                var $divSummary = $('<div id="summary">');
+                var $divDetails = $('<div id="details">');
+                var name = $('<h3>' + response.trails[i].name + '</h3>');
+                var description = $('<p>' + response.trails[i].summary + '</p>');
+                var difficulty = $('<p>Difficulty: ' + response.trails[i].difficulty + '</p>');
+                var length = $('<p>Length: ' + response.trails[i].length + 'mi</p>');
+                var ascent = $('<p>Ascent: ' + response.trails[i].ascent + 'ft</p>');
+                var altitude = $('<p>Highest Point: ' + response.trails[i].high + 'ft</p>');
+                var url = $('<a href="' + response.trails[i].url + '">View Trail Map</a>');
+                $divSummary.append(name, description) 
+                $divDetails.append(difficulty, length, ascent, altitude, url);
+                $trails.append($divSummary, $divDetails);
 
-// function unsplash() {
-//     var input = $('#park-search-val').val();
-//     console.log('unsplash: ' + input);
-
-//     var queryURL = 'https://api.unsplash.com/search/photos?page=1&query=' + input + '&client_id=595205d0fab64dca9acc4912f7319d2869c29ff0834538b31167dbca9425a2f6&client_secret=00c14faa873902ff8dd494014545db17655595508ae0c67fe37a8774e4b7b45c';
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//         }).then(function(response){
-//             console.log(response);
-//             for(var i = 0; i < 1; i++){
-//             $('#search-results').append('<img src="' + response.results[i].urls.regular + '">');
-//             }
-//         });
-
-// };
-
-// function trails() {
-//     var input = latLong.split(',');
-//     console.log(input);
-//     var lat = input[0].substring(4,16);
-//     console.log('latitude: ' + lat);
-//     var long = input[1].substring(6,18);
-//     console.log('longitude: ' + long);
-
-//     $addBtn.attr({'latitude': lat, 'longitude': long});
-
-//     var queryURL = 'https://www.hikingproject.com/data/get-trails?lat='+ lat + '&lon=' + long + '&maxDistance=10&key=200415723-df92bbbf592b6baa4ec5ef44ab0ffed8';
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//         crossOrigin: true,
-//         }).then(function(response){
-//             console.log(response);
-//             for(var i = 0; i < response.trails.length; i++){
-//                 var $trails = $('<div id="trails">');
-
-//                 var $img = $('<img id="trail-img" src="' + response.trails[i].imgSqSmall + '">');
-//                 $trails.append($img);
-
-//                 var $divSummary = $('<div id="summary">');
-//                 var $divDetails = $('<div id="details">');
-//                 var name = $('<h3>' + response.trails[i].name + '</h3>');
-//                 var description = $('<p>' + response.trails[i].summary + '</p>');
-//                 var difficulty = $('<p>Difficulty: ' + response.trails[i].difficulty + '</p>');
-//                 var length = $('<p>Length: ' + response.trails[i].length + 'mi</p>');
-//                 var ascent = $('<p>Ascent: ' + response.trails[i].ascent + 'ft</p>');
-//                 var altitude = $('<p>Highest Point: ' + response.trails[i].high + 'ft</p>');
-//                 var url = $('<a href="' + response.trails[i].url + '">View Trail Map</a>');
-//                 $divSummary.append(name, description) 
-//                 $divDetails.append(difficulty, length, ascent, altitude, url);
-//                 $trails.append($divSummary, $divDetails);
-
-//                 $('#search-results').append($trails);
-//             }
+                $('#search-results').append($trails);
+            }
             
-//         });
-// };
+        });
+};
 
-// $('#park-search-btn').on('click', function(){
-//     event.preventDefault();
-//     parks();
-//     unsplash();
-// });
+$('#park-search-btn').on('click', myFunction);
+
+$('#myDropdown').on('click', 'p.list', function() {
+    parks($(this));
+    unsplash($(this));
+});
 
