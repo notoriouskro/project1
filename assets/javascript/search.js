211515430aadd97cd2bdcefcfaf2934bd7d4d52d
Edward Hopper <burson.kate@gmail.com>
2019-02-07 16:19:06 -0800

    trails api hooked in

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -13,6 +13,7 @@ var database = firebase.database();
 
 // searches to local storage
 var searches = [];
+var latLong = '';
 
 
 
@@ -44,44 +45,81 @@ function parks() {
             $info.append(description, directions, directionsURL, location, weather, website);
 
             $('#search-results').append($name, $info);
+            latLong = response.data[0].latLong;
+
+            trails();
         });
     
 };
 
-$('#park-search-btn').on('click', function(){
-    event.preventDefault();
-    parks();
-});
 
 function unsplash() {
-    var input = $('#input').val().split('');
-    console.log(input);
+    var input = $('#park-search-val').val();
+    console.log('unsplash: ' + input);
 
-    var queryURL = 'https://api.unsplash.com/search/photos?page=1&query=' + input[0] + '&client_id=595205d0fab64dca9acc4912f7319d2869c29ff0834538b31167dbca9425a2f6&client_secret=00c14faa873902ff8dd494014545db17655595508ae0c67fe37a8774e4b7b45c';
+    var queryURL = 'https://api.unsplash.com/search/photos?page=1&query=' + input + '&client_id=595205d0fab64dca9acc4912f7319d2869c29ff0834538b31167dbca9425a2f6&client_secret=00c14faa873902ff8dd494014545db17655595508ae0c67fe37a8774e4b7b45c';
 
     $.ajax({
         url: queryURL,
         method: "GET",
         }).then(function(response){
             console.log(response);
-            for(var i = 0; i < response.results.length; i++){
-            $('#images').append('<img src="' + response.results[i].urls.regular + '">');
+            for(var i = 0; i < 1; i++){
+            $('#search-results').append('<img src="' + response.results[i].urls.regular + '">');
             }
         });
 
 };
 
-// function trails() {
-//     var queryURL = 'https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200415723-df92bbbf592b6baa4ec5ef44ab0ffed8'
+function trails() {
+    var input = latLong.split(',');
+    console.log(input);
+    var lat = input[0].substring(4,16);
+    console.log('latitude: ' + lat);
+    var long = input[1].substring(6,18);
+    console.log('longitude: ' + long);
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//         }).then(function(response){
-//             console.log(response);
-//         });
+    var queryURL = 'https://www.hikingproject.com/data/get-trails?lat='+ lat + '&lon=' + long + '&maxDistance=10&key=200415723-df92bbbf592b6baa4ec5ef44ab0ffed8';
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        crossOrigin: true,
+        }).then(function(response){
+            console.log(response);
+            for(var i = 0; i < response.trails.length; i++){
+                var $trails = $('<div id="trails">');
+
+                var $img = $('<img>');
+                var img = response.trails[i].imgSmall;
+                $img.attr('src="' + img + '">');
+                $trails.append($img);
+
+                var $div = $('<div>');
+                var name = $('<p>' + response.trails[i].name + '</p>');
+                var difficulty = $('<p>' + response.trails[i].difficulty + '</p>');
+                var length = $('<p>' + response.trails[i].length + '</p>');
+                var ascent = $('<p>' + response.trails[i].ascent + '</p>');
+                var altitude = $('<p>' + response.trails[i].high + '</p>');
+                var description = $('<p>' + response.trails[i].summary + '</p>');
+                $div.append(name, difficulty, length, ascent, altitude, description);
+                $trails.append($div);
+
+                var url = response.trails[i].url;
+                var $trailMap = $('<iframe width="600" height="450" frameborder="0" style="border:0" src="' + url +'" allowfullscreen>');
+                $trails.append($trailMap);
+
+                $('#search-results').append($trails);
+            }
+            
+        });
         
-// };
+};
 
 
+$('#park-search-btn').on('click', function(){
+    event.preventDefault();
+    parks();
+    unsplash();
+});
 
