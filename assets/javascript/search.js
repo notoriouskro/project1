c6832693af95f7128420db720b402a31c1ae34ea
Matt Sainson <mattsainson@gmail.com>
2019-02-09 12:01:52 -0800

    Add weather to modal

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -74,6 +74,9 @@ function parks(element) {
 
             $('#itinerary-add-btn').prop('disabled', false);
             
+            console.log('about to call weather');
+            weatherObj.callHomeWeather();
+
         });
 };
 
